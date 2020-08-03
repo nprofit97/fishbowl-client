aa76debc3b0cefa22c9f6cef4d7b941741da150f
Nicole Profit <nprofit97@gmail.com>
2020-08-02 20:26:22 -0400

    determine emoji order

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
 	}
 	
 	renderBackgroundEmojis = () => {
-		let emojis = ['😂', '🐳', '🐠', '🐙', '😂', '🐳', '🐠', '🐙', '😂', '🐳' ]
+		let emojis = ['🤯', '🐳', '🐠', '🐙', '😂', '🐳', '🐠', '🥶', '😂', '🥳' ]
 		return (
 			<div className='area'>
 				<ul className='circles'>{emojis.map((emoji, i) => <li key={i}>{emoji}</li>)}</ul>
